e45c50b28849e5c0796ad6ffffd0aa074463d1e5
Felipe França <felipejonataoliveira@gmail.com>
2021-02-07 09:23:22 -0300

    Creating test for company resources and service and add h2 for junit and mockito tests

--- src/test/java/com/qualstack/service/CompanyServiceTest.java
+++ src/test/java/com/qualstack/service/CompanyServiceTest.java
@@ -1,40 +1,70 @@
 package com.qualstack.service;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.qualstack.model.Company;
-import com.qualstack.repository.CompanyRepository;
 
-@SpringBootTest(classes = Company.class)
+import lombok.Getter;
+import lombok.Setter;
+
+
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@SpringBootTest
 public class CompanyServiceTest {
 
-	@MockBean
-	CompanyRepository companyRepository;
-	
+	private static final String URL = "/company";
+	private static final String NAME = "TESTE";
+	private static final Long ID = 1L;
+
 	@Autowired
+	MockMvc mvc;
+
+	@MockBean
 	CompanyService companyService;
-	
+
 	@Test
-	public void testCreatCompany() {
+	public void testCreateCompany() throws JsonProcessingException, Exception {
+
+		BDDMockito.given(companyService.save(Mockito.any(Company.class))).willReturn(getMockCompany());
+
+		ObjectMapper mapper = new ObjectMapper();
+		
+		mvc.perform(MockMvcRequestBuilders.post(URL).content(mapper.writeValueAsString(getMockCompany()))
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isCreated())
+		.andExpect(jsonPath("$.id").value(ID))
+		.andExpect(jsonPath("$.name").value(NAME));
+
+	}
+
+	@Getter @Setter
+	class CompanyDTO {
+		String name;
+		Long id;
+		
+	}
+	
+	private Company getMockCompany() {
 		Company company = new Company();
-		company.setName("BANCO X");
-		company = companyService.save(company);
-		
-		assertNotEquals(null, company);
-		
+		company.setId(ID);
+		company.setName(NAME);
+		return company;
 	}
-	
-	@BeforeEach
-	public void setUp() {
-		BDDMockito.given(companyRepository.save(Mockito.any(Company.class))).willReturn(new Company(1L));
-	}
-	
 }
